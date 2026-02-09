2a6b7bf0c89fddb75ba884e5333ae28a22fed828
Peixotim <pedropeixotovz@gmail.com>
2026-02-09 16:54:40 -0300

    feat: First steps in infrastructure configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      database: process.env.POSTGRES_DATABASE,
+      type: 'postgres',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      autoLoadEntities: true,
+      synchronize: true, //Alter to false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
