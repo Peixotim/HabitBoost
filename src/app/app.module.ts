c12caf2c92c41b8f3496abdec9c5023bf63502d2
Peixotim <pedropeixotovz@gmail.com>
2026-02-10 11:31:36 -0300

    feat: Adds the import of configDotEnv and adds the host field for database connection with TypeORM.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { configDotenv } from 'dotenv';
+configDotenv({
+  path: '.env',
+});
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST,
       type: 'postgres',
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
