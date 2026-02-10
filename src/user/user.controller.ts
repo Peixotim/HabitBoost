028b82ed1855278d68f42399632acbe84caf56b1
Peixotim <pedropeixotovz@gmail.com>
2026-02-10 14:30:03 -0300

    feat: Creates the user controller with an endpoint for user creation.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UserCreateDTO } from './DTOs/user.create.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/')
+  @HttpCode(HttpStatus.CREATED)
+  public async create(@Body() request: UserCreateDTO) {
+    return await this.userService.create(request);
+  }
+}
