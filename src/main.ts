7b36996194b5620ecfd301e625a19370c273d019
Peixotim <pedropeixotovz@gmail.com>
2026-02-10 11:34:21 -0300

    feat: Adding a log to display the port on which the application is running.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { configDotenv } from 'dotenv';
+
+configDotenv({
+  path: '.env',
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Application is running in port ${process.env.PORT}`);
+  });
 }
 bootstrap();
