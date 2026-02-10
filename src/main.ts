7c14929a337aae351fe0589dbf17dad17ec72e51
Peixotim <pedropeixotovz@gmail.com>
2026-02-10 14:29:19 -0300

    feat: Adds global security and validation middleware.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { configDotenv } from 'dotenv';
-
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 configDotenv({
   path: '.env',
 });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log(`Application is running in port ${process.env.PORT}`);
   });
