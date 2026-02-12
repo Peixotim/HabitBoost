c40593e943fca922d8fbdecb7ca6a80bc2ff9833
Peixotim <pedropeixotovz@gmail.com>
2026-02-11 21:06:32 -0300

    fix: Adjust the import of UserCreateDTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { UserCreateDTO } from './DTOs/user.create.dto';
+import { UserCreateDTO } from './DTOs/user-create.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -9,6 +9,6 @@ export class UserController {
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
   public async create(@Body() request: UserCreateDTO) {
-    return await this.userService.create(request);
+    return await this.userService.create({ ...request });
   }
 }
