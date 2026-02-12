e931e4b46cf02bdcd61f69ccb3bd11d5d83fd517
Peixotim <pedropeixotovz@gmail.com>
2026-02-12 09:25:31 -0300

    feat: Adds methods update/enable/disable , and fix nomeclature findByEmail

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,14 +1,17 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './model/user.model';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { UserCreateDTO } from './DTOs/user-create.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
 export class UserRepository {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
 
-  public async findByMail(email: string) {
+  public async findByEmail(email: string) {
     const user = await this.userRepository.findOne({
       where: { email: email },
     });
@@ -29,6 +32,26 @@ export class UserRepository {
     return await this.userRepository.findOne({
       where: { id: uuid },
     });
+  }
+
+  public async disable(uuid: string) {
+    await this.userRepository.update(uuid, { isActive: false });
+  }
+
+  public async enable(uuid: string) {
+    await this.userRepository.update(uuid, { isActive: true });
+  }
+
+  public async update(uuid: string, data: DeepPartial<User>) {
+    const user = await this.findById(uuid);
+
+    if (!user) {
+      return null;
+    }
+
+    this.userRepository.merge(user, data);
+
+    return this.userRepository.save(user);
   }
 
   public async delete(uuid: string) {
