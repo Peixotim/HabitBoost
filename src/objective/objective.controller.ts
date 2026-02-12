ac01f71569575745c621a07c31bea95b9fd6b724
Peixotim <pedropeixotovz@gmail.com>
2026-02-11 21:25:34 -0300

    feat: Add route getById

--- src/objective/objective.controller.ts
+++ src/objective/objective.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
+import { ObjectiveService } from './objective.service';
 
 @Controller()
-export class ObjectiveController {}
+export class ObjectiveController {
+  constructor(private readonly objectiveService: ObjectiveService) {}
+
+  @Get('/:uuid')
+  @HttpCode(HttpStatus.OK)
+  public async getById(@Param('uuid') uuid: string) {
+    return await this.objectiveService.getById(uuid);
+  }
+}
