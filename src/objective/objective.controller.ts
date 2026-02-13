649a4c0874b01abf6b6378f593630508f7609814
Peixotim <pedropeixotovz@gmail.com>
2026-02-13 11:26:12 -0300

    feat: Adds new controllers of get and post

--- src/objective/objective.controller.ts
+++ src/objective/objective.controller.ts
@@ -5,16 +5,31 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Post,
 } from '@nestjs/common';
 import { ObjectiveService } from './objective.service';
+import { ObjectiveCreateDTO } from './DTOs/objective-create.dto';
+import { Objective } from './model/objective.model';
 
 @Controller()
 export class ObjectiveController {
   constructor(private readonly objectiveService: ObjectiveService) {}
 
+  @Get('/')
+  @HttpCode(HttpStatus.OK)
+  public async getAll(): Promise<Objective[]> {
+    return await this.objectiveService.getAll();
+  }
+
   @Get('/:uuid')
   @HttpCode(HttpStatus.OK)
-  public async getById(@Param('uuid') uuid: string) {
+  public async getById(@Param('uuid') uuid: string): Promise<Objective> {
     return await this.objectiveService.getById(uuid);
+  }
+
+  @Post('/')
+  @HttpCode(HttpStatus.CREATED)
+  public async create(@Body() request: ObjectiveCreateDTO): Promise<Objective> {
+    return await this.objectiveService.create(request);
   }
 }
